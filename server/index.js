9d4e739e45dd6391a7017dec1a799950fe971a1c
carolina-leite <araujoanacarolina@yahoo.com.br>
2022-09-27 22:14:17 -0400

    Instalacao do express e axios

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+
+app.use(cors());
 
 app.use(express.json())
 
@@ -8,8 +10,18 @@ app.get("/health", (req, res) => {
     return res.json("up");
 });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000!');
+//enviar variáveis do front para o back
+app.post('/createCatequizando', (req, res) => {
+  const name = req.body.name
+
+  //db.query('INSERT INTO) //enviar os dados para o db
+})
+
+//app.get('/Catequizandos', (req, res) =>{})
+
+//const db
+app.listen(3001, () => {
+    console.log('Server started on port 3001!');
   });
 
 //import postgres from 'postgres'
