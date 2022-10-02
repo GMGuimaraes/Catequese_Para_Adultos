7e61507acdcfbd88ac2d04d8ce293aad3286b42c
Felipe Lira <felipe.lira.oliveira@gmail.com>
2022-10-01 22:36:37 -0300

    Navbar e Home

---
+++ client/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Home() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
